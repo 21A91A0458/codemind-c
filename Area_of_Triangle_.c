c2190f1a5043946c275afd118e0af773aaabc862
21A91A0458 <98595625+21A91A0458@users.noreply.github.com>
2022-02-18 14:56:41 +0530

    #include<stdio.h> #include<math.h> int main() {     float a,b,c,s;     float area;     scanf("%f%f%f",&a,&b,&c);     s=(a+b+c)/2;     area=sqrt(s*(s-a)*(s-b)*(s-c));     printf("%.2f",area);     return 0; }

--- Area_of_Triangle_.c
+++ Area_of_Triangle_.c
@@ -2,9 +2,11 @@
 #include<math.h>
 int main()
 {
-float a,b,c,s,area;
-scanf("%f%f%f",&a,&b,&c);
-s=(a+b+c)/2;
-area=sqrt(s*(s-a)*(s-b)*(s-c));
-printf("%.2f",area);
+    float a,b,c,s;
+    float area;
+    scanf("%f%f%f",&a,&b,&c);
+    s=(a+b+c)/2;
+    area=sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("%.2f",area);
+    return 0;
 }
